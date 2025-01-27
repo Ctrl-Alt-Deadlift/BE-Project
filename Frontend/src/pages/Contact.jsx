8e480a480c0986079f49cd4d12522a4a1aec4858
ParthByteBard <parthsatvekar2004@gmail.com>
2025-01-28 00:08:43 +0530

    Address fixed

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -14,9 +14,9 @@ const Contact = () => {
       <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
         <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="" />
         <div className="flex flex-col justify-center items-start gap-6">
-          <p className="font-semibold text-xl text-gray-600">Our Store</p>
-          <p className="text-gray-500 ">54709 Williams Station<br />Suite 350, Washigton USA</p>
-          <p className="text-gray-500 ">Tel: (415) 555-0132<br />Email:admit@forever.com</p>
+          <p className="font-semibold text-xl text-gray-600">Our Indian Office</p>
+          <p className="text-gray-500 ">4th floor, 3rd block, Phoniex Tower,<br /> Andheri-West, Mumbai, Maharashtra, India - 400053</p>
+          <p className="text-gray-500 ">Tel: (022)27923456<br />Email:admit@forever.com</p>
           <p className="text-gray-600 text-xl font-semibold ">Careers at Forever</p>
           <p className="text-gray-500 ">Learn more about our teams and job openings.</p>
           <button className="border border-black px-8 py-4 text-md hover:bg-black hover:text-white transition-all duration-500">Explore Jobs</button>
