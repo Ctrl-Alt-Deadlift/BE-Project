725beb38ef45bc288d2440573286dbc8b0118563
ParthByteBard <parthsatvekar2004@gmail.com>
2025-02-22 20:22:35 +0530

    Scroll up functionality added

--- Frontend/src/components/ProductItem.jsx
+++ Frontend/src/components/ProductItem.jsx
@@ -1,22 +1,60 @@
+// /* eslint-disable react/prop-types */
+// import { useContext } from 'react'
+// import { Link } from 'react-router-dom';
+// import { ShopContext } from '../context/ShopContext.jsx'
+
+// const ProductItem = ({ id, image, name, price }) => {
+
+//   const { currency } = useContext(ShopContext);
+//   return (
+//     <div>
+//       <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
+//         <div className='overflow-hidden'>
+//           <img className='hover:scale-110 transition ease-in-out h-[225px]' src={image[0]} alt="" />
+//         </div>
+//         <p className='pt-3 pb-1 text-sm'>{name}</p>
+//         <p className='text-sm font-medium'>{currency}{price}</p>
+//       </Link>
+//     </div>
+//   )
+// }
+
+// export default ProductItem
+
 /* eslint-disable react/prop-types */
-import { useContext } from 'react'
-import { Link } from 'react-router-dom';
-import { ShopContext } from '../context/ShopContext.jsx'
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext.jsx";
 
 const ProductItem = ({ id, image, name, price }) => {
-
   const { currency } = useContext(ShopContext);
+
+  const handleClick = () => {
+    window.scrollTo({ top: 150, behavior: "smooth" });
+  };
+
   return (
     <div>
-      <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
-        <div className='overflow-hidden'>
-          <img className='hover:scale-110 transition ease-in-out h-[225px]' src={image[0]} alt="" />
+      <Link
+        className="text-gray-700 cursor-pointer"
+        to={`/product/${id}`}
+        onClick={handleClick}  // Scroll to top on click
+      >
+        <div className="overflow-hidden">
+          <img
+            className="hover:scale-110 transition ease-in-out h-[225px]"
+            src={image[0]}
+            alt={name}
+          />
         </div>
-        <p className='pt-3 pb-1 text-sm'>{name}</p>
-        <p className='text-sm font-medium'>{currency}{price}</p>
+        <p className="pt-3 pb-1 text-sm">{name}</p>
+        <p className="text-sm font-medium">
+          {currency}
+          {price}
+        </p>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ProductItem
+export default ProductItem;
